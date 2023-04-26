b583b52ac19a44aff96a792e48b893a8c9c2da7e
Bassam Sayed <bassam.sayed@worldcoin.org>
2023-04-26 11:10:39 -0400

    Added Rainbow brackets and Golang, Rust, and Python community packs.

--- plugins/user.lua
+++ plugins/user.lua
@@ -9,4 +9,24 @@ return {
   --     require("lsp_signature").setup()
   --   end,
   -- },
+  {
+    "folke/todo-comments.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    -- config = function()
+    --   require("todo-comments").setup {
+    --     -- your configuration comes here
+    --     -- or leave it empty to use the default settings
+    --     -- refer to the configuration section below
+    --   }
+    -- end
+    opts = {},
+    event = "User AstroFile",
+    cmd = { "TodoQuickFix" },
+  },
+  {
+    "p00f/nvim-ts-rainbow",
+    event = "User AstroFile",
+  },
 }
+
+-- TODO: test
