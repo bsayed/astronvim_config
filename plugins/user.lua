4a43f849cb1755248bcc0b2fa368bb1cbeaf9dd7
Bassam Sayed <bassam.sayed@worldcoin.org>
2023-05-08 15:06:59 -0400

    Added Barbeque bar.

--- plugins/user.lua
+++ plugins/user.lua
@@ -65,6 +65,38 @@ return {
       require("auto-save").setup {
         -- your config goes here
         -- or just leave it empty :)
+        enabled = true,        -- start auto-save when the plugin is loaded (i.e. when your package manager loads it)
+        execution_message = {
+          message = function() -- message to print on save
+            return ("AutoSave: saved at " .. vim.fn.strftime "%H:%M:%S")
+          end,
+          dim = 0.18,               -- dim the color of `message`
+          cleaning_interval = 1250, -- (milliseconds) automatically clean MsgArea after displaying `message`. See :h MsgArea
+        },
+        -- trigger_events = { "InsertLeave", "TextChanged" }, -- vim events that trigger auto-save. See :h events
+        -- trigger_events = { "BufWinLeave" }, -- vim events that trigger auto-save. See :h events
+        -- function that determines whether to save the current buffer or not
+        -- return true: if buffer is ok to be saved
+        -- return false: if it's not ok to be saved
+        condition = function(buf)
+          local fn = vim.fn
+          local utils = require "auto-save.utils.data"
+
+          if fn.getbufvar(buf, "&modifiable") == 1 and utils.not_in(fn.getbufvar(buf, "&filetype"), {}) then
+            return true            -- met condition(s), can save
+          end
+          return false             -- can't save
+        end,
+        write_all_buffers = false, -- write all buffers when the current one meets `condition`
+        debounce_delay = 5000,     -- saves the file at most every `debounce_delay` milliseconds
+        callbacks = {
+          -- functions to be executed at different intervals
+          enabling = nil,              -- ran when enabling auto-save
+          disabling = nil,             -- ran when disabling auto-save
+          before_asserting_save = nil, -- ran before checking `condition`
+          before_saving = nil,         -- ran before doing the actual save
+          after_saving = nil,          -- ran after doing the actual save
+        },
       }
     end,
   },
@@ -72,6 +104,19 @@ return {
     "simrat39/symbols-outline.nvim",
     event = "User AstroFile",
     config = function() require("symbols-outline").setup() end,
+  },
+  {
+    "utilyre/barbecue.nvim",
+    name = "barbecue",
+    version = "*",
+    event = "User AstroFile",
+    dependencies = {
+      "SmiteshP/nvim-navic",
+      "nvim-tree/nvim-web-devicons", -- optional dependency
+    },
+    opts = {
+      -- configurations go here
+    },
   },
 }
 
