33259710545841da435af5f093c1a340ad13b17d
Bassam Sayed <bassam.sayed@worldcoin.org>
2023-08-03 08:59:27 -0400

    Added motion and aerial outline plugins

--- plugins/user.lua
+++ plugins/user.lua
@@ -65,12 +65,12 @@ return {
       require("auto-save").setup {
         -- your config goes here
         -- or just leave it empty :)
-        enabled = true,        -- start auto-save when the plugin is loaded (i.e. when your package manager loads it)
+        enabled = true, -- start auto-save when the plugin is loaded (i.e. when your package manager loads it)
         execution_message = {
           message = function() -- message to print on save
             return ("AutoSave: saved at " .. vim.fn.strftime "%H:%M:%S")
           end,
-          dim = 0.18,               -- dim the color of `message`
+          dim = 0.18, -- dim the color of `message`
           cleaning_interval = 1250, -- (milliseconds) automatically clean MsgArea after displaying `message`. See :h MsgArea
         },
         -- trigger_events = { "InsertLeave", "TextChanged" }, -- vim events that trigger auto-save. See :h events
@@ -84,19 +84,19 @@ return {
           local utils = require "auto-save.utils.data"
 
           if fn.getbufvar(buf, "&modifiable") == 1 and utils.not_in(fn.getbufvar(buf, "&filetype"), {}) then
-            return true            -- met condition(s), can save
+            return true -- met condition(s), can save
           end
-          return false             -- can't save
+          return false -- can't save
         end,
         write_all_buffers = false, -- write all buffers when the current one meets `condition`
-        debounce_delay = 5000,     -- saves the file at most every `debounce_delay` milliseconds
+        debounce_delay = 5000, -- saves the file at most every `debounce_delay` milliseconds
         callbacks = {
           -- functions to be executed at different intervals
-          enabling = nil,              -- ran when enabling auto-save
-          disabling = nil,             -- ran when disabling auto-save
+          enabling = nil, -- ran when enabling auto-save
+          disabling = nil, -- ran when disabling auto-save
           before_asserting_save = nil, -- ran before checking `condition`
-          before_saving = nil,         -- ran before doing the actual save
-          after_saving = nil,          -- ran after doing the actual save
+          before_saving = nil, -- ran before doing the actual save
+          after_saving = nil, -- ran after doing the actual save
         },
       }
     end,
@@ -144,10 +144,26 @@ return {
       "neovim/nvim-lspconfig",
       "SmiteshP/nvim-navic",
       "MunifTanjim/nui.nvim",
-      "numToStr/Comment.nvim",         -- Optional
+      "numToStr/Comment.nvim", -- Optional
       "nvim-telescope/telescope.nvim", -- Optional
     },
     opts = { lsp = { auto_attach = true } },
+  },
+  {
+    "chaoren/vim-wordmotion",
+    dependencies = "nvim-treesitter/nvim-treesitter",
+    event = "User AstroFile",
+    version = "*",
+  },
+  {
+    "stevearc/aerial.nvim",
+    opts = {},
+    event = "User AstroFile",
+    -- Optional dependencies
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter",
+      "nvim-tree/nvim-web-devicons",
+    },
   },
 }
 
