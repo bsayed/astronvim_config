a63443e8ee59158e6c8af95a7289ac5ab50b0ca9
Bassam Sayed <bassam.sayed@worldcoin.org>
2023-08-31 12:48:57 -0400

    latest working changes

--- plugins/user.lua
+++ plugins/user.lua
@@ -65,15 +65,15 @@ return {
   },
   {
     "https://git.sr.ht/~nedia/auto-save.nvim",
-    event = "BufWinEnter",
+    event = "User AstroFile",
     config = function()
       require("auto-save").setup {
         -- The name of the augroup.
         augroup_name = "AutoSavePlug",
 
         -- The events in which to trigger an auto save.
-        -- events = { "InsertLeave", "TextChanged" },
-        events = { "InsertLeave", "BufLeave" },
+        events = { "InsertLeave", "TextChanged", "BufLeave" },
+        -- events = { "InsertLeave", "BufLeave" },
 
         -- If you'd prefer to silence the output of `save_fn`.
         silent = false,
@@ -83,14 +83,14 @@ return {
 
         -- What to do after checking if auto save conditions have been met.
         save_fn = function()
-          local config = require "auto-save.config"
-          if config.save_cmd ~= nil then
-            vim.cmd(config.save_cmd)
-          -- elseif M.silent then
-          --   vim.cmd "silent! w"
-          else
-            vim.cmd "w"
-          end
+          -- local config = require "auto-save.config"
+          -- if config.save_cmd ~= nil then
+          --   vim.cmd(config.save_cmd)
+          -- -- elseif M.silent then
+          -- --   vim.cmd "silent! w"
+          -- else
+          vim.cmd "w"
+          -- end
         end,
 
         -- May define a timeout, or a duration to defer the save for - this allows
